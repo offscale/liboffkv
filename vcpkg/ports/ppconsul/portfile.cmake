9704da6619e17d52442965d64e6108501fdcb921
Dmitrii Kamaldinov <dimak24.05@mail.ru>
2019-04-27 00:11:42 +0300

    vcpkg

---
+++ vcpkg/ports/ppconsul/portfile.cmake
@@ -0,0 +1,21 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO oliora/ppconsul
+    REF e32cf6f80b598760278c018eed546f88292f1735
+    SHA512 8ca3b1f37a22bf27ce27af27676ba88a67c0c7b3357488edb925649b5d59b546b5cbf7a14452d07b41bb2649de7d0a8126d247a797f1a0e7395c3704fb54a758
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
+
+file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/ppconsul RENAME copyright)
+
+vcpkg_copy_pdbs()
---
+++ vcpkg/ports/zkpp/portfile.cmake
@@ -0,0 +1,28 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO tgockel/zookeeper-cpp
+    REF v0.2.2
+    SHA512 64302e69e106cf314aa1861e79aaad175cec8f821b7b2ead17075b5db06a8a7484d723dae482ebf6c3185a0a8d57a915081dfbf399cd72eeeb90f58f01d94b3c
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_build_cmake(LOGFILE_ROOT install TARGET all ${ARGN})
+
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/zkpp RENAME copyright)
+
+file(GLOB libraries "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/*.so*")
+file(COPY ${libraries} DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
+
+file(GLOB zk "${SOURCE_PATH}/src/zk/*.hpp")
+file(GLOB zk_server "${SOURCE_PATH}/src/zk/server/*.hpp")
+file(COPY ${zk} DESTINATION ${CURRENT_PACKAGES_DIR}/include/zk/)
+file(COPY ${zk_server} DESTINATION ${CURRENT_PACKAGES_DIR}/include/zk/server/)
+
+vcpkg_copy_pdbs()
