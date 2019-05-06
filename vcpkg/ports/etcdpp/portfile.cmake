c2d78b6f2140e07f66c67310607b8bee87bd6841
Raed Romanov <raid_r@mail.ru>
2019-05-06 19:49:56 +0300

    Update etcdpp vcpkg

--- vcpkg/ports/etcdpp/portfile.cmake
+++ vcpkg/ports/etcdpp/portfile.cmake
@@ -1,16 +1,15 @@
 include(vcpkg_common_functions)
 
-set(VCPKG_BUILD_TYPE release)
+# set(VCPKG_BUILD_TYPE release)
 
-vcpkg_from_github(
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO nokia/etcd-cpp-apiv3
-    REF master
-    SHA512 f16d38e527f91adf7a5859c73efe281deb307ffed38e1ddef64671e258fcc77d75566ebb05817a77e05a0f098bb1159488f89106a4840da568936e9158f23505
-    HEAD_REF master
+    URL git@github.com:SamuelMarks/libetcd-cpp.git
+    REF 65a2fe55d427fb6511c419e34b607b57d667ec67
+    SHA512 db5aa4365eadbe3ff9f1549a35162c197fa821f4bf759a568694747aed1dda4902852a2472f22bb43e2ffea48698653e94be18503e9ac55691a8421ce8e23e7b
 )
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION ${SOURCE_PATH})
+# file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
