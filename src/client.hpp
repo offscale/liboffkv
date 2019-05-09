f166d37bc7fa5ea959863dea915882e314c20a15
Raed Romanov <raid_r@mail.ru>
2019-05-09 18:06:09 +0300

    Add cmake configuration options for selecting supported services

--- src/client.hpp
+++ src/client.hpp
@@ -1,30 +1,47 @@
-
 #pragma once
 
 #include <string>
+#include "config.h"
 
-
+#ifdef ENABLE_CONSUL
 #include "consul_client.hpp"
-#include "error.hpp"
+#endif
+
+#ifdef ENABLE_ETCD
 #include "etcd_client.hpp"
+#endif
+
+#ifdef ENABLE_ZK
+#include "zk_client.hpp"
+#endif
+
+#include "client_interface.hpp"
+#include "error.hpp"
 #include "time_machine.hpp"
 #include "util.hpp"
-#include "zk_client.hpp"
 
 
 
-template <typename TimeMachine = time_machine::ThreadPool<>>
-std::unique_ptr<Client<TimeMachine>> connect(const std::string& address,
-                                             std::shared_ptr<TimeMachine> tm = nullptr)
+template <typename ThreadPool = time_machine::ThreadPool<>>
+std::unique_ptr<Client<ThreadPool>> connect(const std::string& address,
+                                             std::shared_ptr<ThreadPool> tm = nullptr)
 {
     auto [protocol, host_port] = get_protocol_address(address);
 
+#ifdef ENABLE_ZK
     if (protocol == "zk")
-        return std::make_unique<ZKClient<TimeMachine>>(protocol + "://" + host_port, std::move(tm));
+        return std::make_unique<ZKClient<ThreadPool>>(protocol + "://" + host_port, std::move(tm));
+#endif
+
+#ifdef ENABLE_CONSUL
     if (protocol == "consul")
-        return std::make_unique<ConsulClient<TimeMachine>>(host_port, std::move(tm));
+        return std::make_unique<ConsulClient<ThreadPool>>(host_port, std::move(tm));
+#endif
+
+#ifdef ENABLE_ETCD
     if (protocol == "etcd")
-        return std::make_unique<ETCDClient<TimeMachine>>(host_port, std::move(tm));
-    else
-        throw InvalidAddress{};
+        return std::make_unique<ETCDClient<ThreadPool>>(host_port, std::move(tm));
+#endif
+
+    throw InvalidAddress{};
 }
