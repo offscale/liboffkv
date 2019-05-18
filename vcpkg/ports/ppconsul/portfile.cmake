3e08348633436570b05b93079efc82842d9a82c2
Raed Romanov <raid_r@mail.ru>
2019-05-18 13:54:59 +0300

    ppconsul port: disable debug build

--- vcpkg/ports/ppconsul/portfile.cmake
+++ vcpkg/ports/ppconsul/portfile.cmake
@@ -1,5 +1,7 @@
 include(vcpkg_common_functions)
 
+set(VCPKG_BUILD_TYPE release)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO shdown/ppconsul
@@ -18,7 +20,7 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
 
 
 file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/ppconsul RENAME copyright)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/share/ppconsul/ppconsulConfig-debug.cmake)
+# file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+# file(REMOVE ${CURRENT_PACKAGES_DIR}/share/ppconsul/ppconsulConfig-debug.cmake)
 
 vcpkg_copy_pdbs()
