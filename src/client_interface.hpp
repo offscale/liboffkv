430cf01804db12b0ef5099ae2fb2d661c9d14473
Dmitrii Kamaldinov <dimak24.05@mail.ru>
2019-05-23 23:35:15 +0300

    docs in client_interface.hpp

--- src/client_interface.hpp
+++ src/client_interface.hpp
@@ -17,12 +17,11 @@ protected:
     std::string address_;
     std::string prefix_;
 
-public:
     Client(std::shared_ptr<ThreadPool> time_machine = nullptr)
         : thread_pool_(time_machine == nullptr ? std::make_shared<ThreadPool>() : std::move(time_machine))
     {}
 
-    Client() = default;
+public:
 
     virtual
     ~Client() = default;
@@ -32,27 +31,83 @@ public:
         return address_;
     }
 
+
+    /*
+     * Create the key.
+     * Throw EntryExists if the key already exists.
+     * Throw NoEntry if preceding entry does not exist.
+     */
     virtual
     std::future<void> create(const std::string& key, const std::string& value, bool lease = false) = 0;
 
+
+    /*
+     * Check if the key exists.
+     */
     virtual
     std::future<ExistsResult> exists(const std::string& key, bool watch = false) = 0;
 
+
+    /*
+     * Get a list of key's children
+     * Throw NoEntry, if the key does not exist.
+     *
+     * Assign watch to the key if <watch> is true
+     *
+     *
+     * get_children("/foo") -> {"/foo/bar", "foo/baz"}
+     * get_children("/foo/bar") -> {"/foo/bar/var"}
+     *
+     */
     virtual
     std::future<ChildrenResult> get_children(const std::string& key, bool watch = false) = 0;
 
+
+    /*
+     * Set the value and to the key.
+     * Create the key if it doesn’t exist.
+     * Throw NoEntry if preceding entry does not exist.
+     *
+     * Assign watch to the key if <watch> is true
+     */
     virtual
     std::future<SetResult> set(const std::string& key, const std::string& value) = 0;
 
+
+    /*
+     * Compare and set operation.
+     *
+     * If the key does not exist and version equals 0 create it.
+     * Throw NoEntry if preceding entry does not exist.
+     *
+     * If the key exists and its version equals specified version update value.
+     * Otherwise do nothing.
+     */
     virtual
     std::future<CASResult> cas(const std::string& key, const std::string& value, uint64_t version = 0) = 0;
 
+
+    /*
+     * Throw NoEntry if the key does not exist.
+     */
     virtual
     std::future<GetResult> get(const std::string& key, bool watch = false) = 0;
 
+
+    /*
+     * Delete the key, if its version is equal to specified version or specified version equals 0.
+     * Throw NoEntry, if the key does not exist.
+     *
+     * Assign watch to the key if <watch> is true
+     */
     virtual
     std::future<void> erase(const std::string& key, uint64_t version = 0) = 0;
 
+
+    /*
+     * Perform transaction.
+     * Throw TransactionFailed if it fails
+     */
     virtual
     std::future<TransactionResult> commit(const Transaction&) = 0;
 };
