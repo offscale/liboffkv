38e55fb0ef5fe5ad8d1a7ac8e4c3608d8176a15c
Raed Romanov <raid_r@mail.ru>
2019-05-24 19:53:32 +0300

    Use vcpkg curl in ppconsul

--- vcpkg/ports/ppconsul/portfile.cmake
+++ vcpkg/ports/ppconsul/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
     REF 39520bdc5d5910cc63a10ab6187821dd11e97bea
     SHA512 d65cc382c2328651fb7409316365ec0ab6675e6a73e3217f0fdfc6d3439fd40faf6b92ef4aa2464063e01f9697d1bff2d196d69f3fc2c9afc296d6f786db6bfa
     HEAD_REF master
+    PATCHES "cmake_build.patch"
 )
 
 vcpkg_configure_cmake(
