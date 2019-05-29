41cb40730277ec0e83bd22fd19d6f651b7a8ee41
Raed Romanov <raid_r@mail.ru>
2019-05-29 12:22:31 +0300

    Fix etcdpp Windows build

--- vcpkg/ports/etcdpp/portfile.cmake
+++ vcpkg/ports/etcdpp/portfile.cmake
@@ -1,15 +1,12 @@
 include(vcpkg_common_functions)
 
-# set(VCPKG_BUILD_TYPE release)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO raid-7/libetcd-cpp
-    REF 20db6e3a941c421e3d50d1ad5dd66197bd8f7e4c
-    SHA512 06aaa6b8737b4df5af97345669c12fa5288003958a7591840d31ce0b506ba3a415b0491d392033a967b7fbc44b7a8ec33e442baa3cd0a2ec97c03697ad581123
+    REF fcb2afbd0e47e67aedbd1fee06ee132fc9a84c53
+    SHA512 459507ef8543657156822f208e709f013d2b4fb22736946de4295b96c7da85823829467ca2308168f96ee96cb94d20d53740598a6624fa2cbf78fd2d9d5dda54
+    HEAD_REF master
 )
-
-# file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
--- vcpkg/ports/ppconsul/portfile.cmake
+++ vcpkg/ports/ppconsul/portfile.cmake
@@ -1,6 +1,6 @@
 include(vcpkg_common_functions)
 
-set(VCPKG_BUILD_TYPE release)
+# set(VCPKG_BUILD_TYPE release)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -21,7 +21,9 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
 
 
 file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/ppconsul RENAME copyright)
-# file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 # file(REMOVE ${CURRENT_PACKAGES_DIR}/share/ppconsul/ppconsulConfig-debug.cmake)
 
 vcpkg_copy_pdbs()
+
+message(STATUS "Type: ${VCPKG_BUILD_TYPE}")
