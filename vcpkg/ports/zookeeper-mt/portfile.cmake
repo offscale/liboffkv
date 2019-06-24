2279ca63dbe8155294a3e7da7b5429d5f887072b
Raed Romanov <raid_r@mail.ru>
2019-06-24 17:31:26 +0300

    Update zk

--- vcpkg/ports/zookeeper-mt/portfile.cmake
+++ vcpkg/ports/zookeeper-mt/portfile.cmake
@@ -1,30 +1,18 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://archive.apache.org/dist/zookeeper/zookeeper-3.5.4-beta/zookeeper-3.5.4-beta.tar.gz"
-    FILENAME "zookeeper-3.5.4-beta.tar.gz"
-    SHA512 3b45ea03f1f710310633141e5190cd9c7d734d0075bde996c82fe1e4e40a86a2aa48c41412ac7d7fd71c68b2f4c2497482d321b495b7283a57c9a2f0fba9a62e
+    URLS "https://archive.apache.org/dist/zookeeper/zookeeper-3.5.5/apache-zookeeper-3.5.5.tar.gz"
+    FILENAME "zookeeper-3.5.5.tar.gz"
+    SHA512 4e22df899a83ca3cc15f6d94daadb1a8631fb4108e67b4f56d1f4fcf95f10f89c8ff1fb8a7c84799a3856d8803a8db1e1f2f3fe1b7dc0d6cedf485ef90fd212d
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE} 
+    PATCHES "cmake_build.patch"
 )
 
-set(SOURCE_PATH ${SOURCE_PATH}/src/c)
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+set(SOURCE_PATH ${SOURCE_PATH}/zookeeper-client/zookeeper-client-c)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -34,3 +22,5 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/zookeeper-mt RENAME copyright)
+
+vcpkg_copy_pdbs()
